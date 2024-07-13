cb391de06f78af68c454f178664fea486ac81e21
Marxu <fuhaojun@52tt.com>
2024-07-13 14:15:17 +0800

    feat: create a form model 拿到请求的数据

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+            print(first_name)
+
+    return render(request, 'index.html')
